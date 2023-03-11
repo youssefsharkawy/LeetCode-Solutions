5058633c5af1027c0f208b265cda879220f146df
Youssef Sharkawy <youssefsharkawy22@gmail.com>
2023-03-11 13:09:12 +0200

    Two Sum Problem Solution

---
+++ TwoSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+           
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                    
+                }
+                
+                
+            }
+            return nums;
+        }
+    }
+}
